6507836858f7482a80b1f55f5aff897583cdd037
Hamzahbay <bayagubhamzah@gmail.com>
2023-06-16 19:58:17 +0700

    init

--- Routers/product.js
+++ Routers/product.js
@@ -12,14 +12,14 @@ router.get('/addStock', authentication.ensure, controller.get.addStock)
 router.get('/editStock', authentication.ensure, controller.get.editStock)
 router.get('/updateStock', authentication.ensure, controller.get.updateStock)
 router.get('/addWarehouse', authentication.ensure, controller.get.addWarehouse)
+router.get('/warehouse/edit', authentication.ensure, controller.get.editWarehouse)
 router.get('/warehouse/:id', authentication.ensure, controller.get.warehouse)
 router.get('/warehouseDetail/:id', authentication.ensure, controller.get.warehouseDetail)
-router.get('/warehouse/edit', authentication.ensure, controller.get.editWarehouse)
 
 // post handling
 router.post('/newStock', authentication.ensure, controller.post.newStock)
 router.post('/updateStock/:id', authentication.ensure, controller.post.updateStock)
-router.post('/warehouseDetail/:id/:productId', authentication.ensure, controller.post.warehouse)
 router.post('/warehouse/edit/:newAccount', authentication.ensure, controller.post.editWarehouse)
+router.post('/warehouseDetail/:id/:productId', authentication.ensure, controller.post.warehouse)
 
 module.exports = router
